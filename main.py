477c1eb8be3bedaff91f995f9da2c95724fc23db
antoine <antoine@antoine>
2021-09-12 19:17:43 +0200

    Mise en place de fonctions. Ca sera indispensable pour la suite

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python3
 
-f = open('map.txt','r')
-count = 0
-Lines = f.readlines()
-for line in Lines:
-	count += 1
-	#print("Line{}: {}".format(count, line.strip()))
+def recherche(nom):
+	firstline = open('map.txt', 'r')
+	file_content = firstline.readlines()
+	firstline.close()
+	line_zero = file_content[0]
 	i = 0
-	for i in range(len(line.strip())):
-		if(line.strip()[i] == "."):
-			
+	path = ""
+	for i in range(len(file_content[0])):#De 0 jusqu'à la longueur totale de la ligne. Scan
+		if (line_zero[i] == "."):
+			path = path + str(i)
 		else:
-			print("")
+			pass
+	print("Le bot",nom,"a trouvé",len(path),"chemins !")
+	return path
+
+def chemins_affichage(path):
+	i = 0
+	printable_path = ""
+	for i in range(len(path)):
+		printable_path = printable_path + path[i] + " "
+	return printable_path
+
+#bot_1
+pos_x = 0
+pos_y = 0
+nom = "searcher"
+path = recherche(nom)#Fonction de scan de la première couche
+print("effectivement, les chemins",chemins_affichage(path), "sont à explorer")
